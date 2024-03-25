4f96bbf4d5463fdad5d612c65664db8c0e05909b
Nashaat Alkean <143865636+NashaatAlkean@users.noreply.github.com>
2024-03-25 17:29:31 +0200

    add the forget password option with all the functionality

--- example/settings.py
+++ example/settings.py
@@ -124,6 +124,16 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Gmail's SMTP server
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ecoexchange.email@gmail.com'
+EMAIL_HOST_PASSWORD = 'vmpp eduo zwje dftj'
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
--- example/urls.py
+++ example/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',include('website.urls')),
@@ -27,6 +28,10 @@ urlpatterns = [
     path('dashboard/',include('dashboard.urls')),
     path('items/',include('items.urls')),
     path('inbox/',include('conversation.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='reset_password\password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password\password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reset_password\password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='reset_password\password_reset_complete.html'), name='password_reset_complete'),
 
 
 
